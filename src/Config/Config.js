af2309a6859c4e3ebedf2d886776ca038f60575e
doyindev1 <ade.doyin27@gmail.com>
2022-09-03 21:39:05 +0100

    fx: CORS ERROR successfully bypassed

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,6 +1,7 @@
 const prod = {
 	url: {
-		API_URL: 'https://fakestoreapi.com',
+		API_URL: // https://afternoon-ridge-35420.herokuapp.com was used to bypass the CORS ERROR 
+		'https://afternoon-ridge-35420.herokuapp.com/https://fakestoreapi.com',
 
 	},
 };
